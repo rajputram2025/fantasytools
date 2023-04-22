e900db12a59011b5b6bdc36d71bca0c41e6490a8
unknown <rampratap.singh@thenudge.org>
2023-04-23 03:26:37 +0530

    update

--- StreamLit/UI.py
+++ StreamLit/UI.py
@@ -9,6 +9,10 @@ from gsheetsdb import connect
 
 st.set_page_config(layout="wide")
 
+footer="<style> your css code put here</style><div class='footer'><p>the word you want to tell<a style='display:block;text-align:center;' href='https://www.streamlit.io' target='_blank'>your email address put here</a></p></div>"
+
+st.markdown(footer, unsafe_allow_html=True)
+
 def RemoveDuplicate(listToRemoveDuplicates):
     return list(set(listToRemoveDuplicates))
 
